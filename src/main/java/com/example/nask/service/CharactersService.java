a003b9b90a4d58d4bc3f41c93f27e570228975df
Sylwester <Sylwesterl445@gmail.com>
2021-07-10 19:11:04 +0200

    Create Service

--- src/main/java/com/example/nask/service/CharactersService.java
+++ src/main/java/com/example/nask/service/CharactersService.java
@@ -1,4 +1,24 @@
 package com.example.nask.service;
 
+import com.example.nask.components.SwapiWebClient;
+import com.example.nask.model.AllCharactersResponse;
+import com.example.nask.model.CharacterResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CharactersService {
+
+    private final SwapiWebClient swapiWebClient;
+
+    public CharactersService(SwapiWebClient swapiWebClient) {
+        this.swapiWebClient = swapiWebClient;
+    }
+
+    public AllCharactersResponse getCharacters(Integer page) {
+        return swapiWebClient.CreateAllCharactersResponse(page);
+    }
+
+    public CharacterResponse getCharacterById(Integer id) {
+        return swapiWebClient.getCharacter(id);
+    }
 }
