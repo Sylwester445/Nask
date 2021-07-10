f956b7e9e20d20632eae9ffdbbfa26402cb0cc18
Sylwester <Sylwesterl445@gmail.com>
2021-07-10 22:13:15 +0200

    enable caching

--- src/main/java/com/example/nask/NaskApplication.java
+++ src/main/java/com/example/nask/NaskApplication.java
@@ -2,7 +2,9 @@ package com.example.nask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class NaskApplication {
 
