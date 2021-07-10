983a9e7924e0858c48794b70bb8a519731924e63
Sylwester <Sylwesterl445@gmail.com>
2021-07-10 18:30:37 +0200

    Add a Spring Project (Spring initializr)

---
+++ src/main/java/com/example/nask/NaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.nask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nask/NaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
