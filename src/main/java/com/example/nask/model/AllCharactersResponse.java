5071e205be81db2e63da129c3ba31ece7a7735c5
Sylwester <Sylwesterl445@gmail.com>
2021-07-10 19:25:11 +0200

    Create Response models

--- src/main/java/com/example/nask/model/AllCharactersResponse.java
+++ src/main/java/com/example/nask/model/AllCharactersResponse.java
@@ -1,4 +1,55 @@
 package com.example.nask.model;
 
+import java.util.List;
+
 public class AllCharactersResponse {
+
+    private String next;
+    private String previous;
+
+    private Integer count;
+    private Integer pages;
+    private List<CharacterResponse> elements;
+
+    /*getters and setters*/
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
+    }
+
+    public List<CharacterResponse> getElements() {
+        return elements;
+    }
+
+    public void setElements(List<CharacterResponse> elements) {
+        this.elements = elements;
+    }
 }
--- src/main/java/com/example/nask/model/CharacterResponse.java
+++ src/main/java/com/example/nask/model/CharacterResponse.java
@@ -1,4 +1,36 @@
 package com.example.nask.model;
 
+import java.util.List;
+
 public class CharacterResponse {
+
+    private SwapiCharacter character;
+    private SwapiHomeworld homeworld;
+    private List<SwapiStarship> starships;
+
+    /*getters and setters*/
+
+    public SwapiCharacter getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(SwapiCharacter character) {
+        this.character = character;
+    }
+
+    public SwapiHomeworld getHomeworld() {
+        return homeworld;
+    }
+
+    public void setHomeworld(SwapiHomeworld homeworld) {
+        this.homeworld = homeworld;
+    }
+
+    public List<SwapiStarship> getStarships() {
+        return starships;
+    }
+
+    public void setStarships(List<SwapiStarship> starships) {
+        this.starships = starships;
+    }
 }
